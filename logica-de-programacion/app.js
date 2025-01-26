8171c4f1e18ad2758f40bc32d630d5469f414d6d
Anthony <b3asbe@gmail.com>
2025-01-26 01:18:44 -0500

    Add carpeta logica programación

---
+++ logica-de-programacion/app.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Juego del calamar ";
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Ingresa un número del 1 al 10";
+
+
+function intentoDeUsuario()
+{
+    alert('Click desde el botón')
+}
